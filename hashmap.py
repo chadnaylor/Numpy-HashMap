19d1169984b69937097ca2b291329cd4c56cbd90
Chad Naylor <pair+chad@chadnaylor.org>
2021-05-01 14:03:20 -0500

    initial

---
+++ hashmap.py
@@ -0,0 +1,23 @@
+from collections.abc import MutableMapping
+
+class HashMap(MutableMapping):
+  def __init__(self):
+    pass
+
+  def __del__(self):
+    pass
+
+  def __len__(self):
+    pass
+
+  def __setitem__(self, key, value):
+    pass
+
+  def __getitem__(self, key):
+    pass
+
+  def __delitem__(self, key):
+    pass
+
+  def __contains__(self, key):
+    pass

0e3f257064c367f02a006c73e327b5068c5f6429
Chad Naylor <pair+chad@chadnaylor.org>
2021-05-01 21:00:37 -0500

    add, get, init, and contains work

--- hashmap.py
+++ hashmap.py
@@ -1,10 +1,19 @@
 from collections.abc import MutableMapping
 import numpy as np
+from get_hash import get_hash
+from item import Item
 
 class HashMap(MutableMapping):
-  def __init__(self, *args, **kwargs):
-    self._size = 2**10 #initialize array length
-    self._array = np.zeros(self._size, dtype=object) #create array
+  def __init__(self, *args):
+    self.__size = 2**8 #initialize array length
+    self.__array = np.zeros(self.__size, dtype=object) #create empty array
+
+    for arg in args:
+      if type(arg) is not dict:
+        raise TypeError("HashMap must be initialized empty or with a dict")
+
+      for key, value in arg.items():
+        self.__setitem__(key, value)
 
   def __del__(self):
     pass
@@ -13,20 +22,50 @@ class HashMap(MutableMapping):
     pass
 
   def __setitem__(self, key, value):
-    pass
+    h = get_hash(self.__size, key)
+
+    if self.__array[h] == 0:
+      self.__array[h] = Item(key, value)
+
+    else:
+      curr_item = self.__array[h]
+      while curr_item.key != key and curr_item.chain is not None:
+        curr_item = curr_item.chain
+      curr_item.chain = Item(key, value)     
 
   def __getitem__(self, key):
-    pass
+    h = get_hash(self.__size, key)
+    if self.__array[h] == 0:
+      raise NameError(f"{key} does not exist!")
+    else:
+      curr_item = self.__array[h]
+      while curr_item.key != key and curr_item.chain is not None:
+        curr_item = curr_item.chain
+
+      if curr_item.key is not key:
+        raise NameError(f"{key} does not exist!")
+      else:
+        return curr_item.value
 
   def __delitem__(self, key):
     pass
 
   def __contains__(self, key):
-    pass
+    h = get_hash(self.__size, key)
+    if self.__array[h] == 0:
+      return False
+    else:
+      curr_item = self.__array[h]
+      while curr_item.key != key and curr_item.chain is not None:
+        curr_item = curr_item.chain
+
+      if curr_item.key is not key:
+        return False
+      else:
+        return True
 
   def __iter__(self):
     pass
 
-  def _handle_collision(self):
-    pass
+
 
--- hashmap_test.py
+++ hashmap_test.py
@@ -4,15 +4,28 @@ from hypothesis import given, strategies as st
 import test_helpers
 
 @given(test_helpers.RANDOM_DICTS)
-def test_fuzz_HashMap(d):
-  HashMap(*d)
+def test_init_from_dict(d):
   HashMap(d)
 
-@given(test_helpers.RANDOM_KEYS)
-def test_getitem(i):
-  hashmap = HashMap()
+@given(st.one_of(st.integers(), st.booleans(), st.text(), st.floats())) 
+def test_init_other_types(args):
+  with pytest.raises(TypeError):
+    HashMap(args)
 
+@given(test_helpers.RANDOM_KEYS)
+def test_getitem_dne(i):
+  hashmap = HashMap()
   with pytest.raises(NameError):
     hashmap[i]
 
-  
+@given(init=test_helpers.RANDOM_DICTS)
+def test_getitem(init):
+  hashmap = HashMap(init)
+  for item in init:
+    assert hashmap[item] == init[item]
+
+@given(init=test_helpers.RANDOM_DICTS)
+def test_contains(init):
+  hashmap = HashMap(init)
+  for item in init:
+    assert item in hashmap
